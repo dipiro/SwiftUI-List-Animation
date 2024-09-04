33ac50d4f77243771332c0ae28fde44af2609248
dipiro <d1monsi10@gmail.com>
2024-09-04 11:55:36 -0400

    add 3D Planet Animation

---
+++ 2_3D_Planet_Animation/SwiftUI_3DPlantet/SwiftUI_3DPlantetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_3DPlantetApp.swift
+//  SwiftUI_3DPlantet
+//
+//  Created by piro2 on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_3DPlantetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
